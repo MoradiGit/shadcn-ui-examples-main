19cd57f8c171a7e4d8c45151c5104fdf712e4a4c
Ahmad <ahmadmoradi58@gmail.com>
2025-09-06 15:52:52 +0330

    feat: add Yekan Persian font

--- app/examples/authentication/page.tsx
+++ app/examples/authentication/page.tsx
@@ -30,7 +30,7 @@ export default function AuthenticationPage() {
           className="hidden dark:block"
         />
       </div> */}
-      <div dir="ltr" className="container relative  h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div dir="ltr" className="container relative  h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0 " >
 
         <div className="relative  h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
